cf98379f6cbab3184f95021fcb81b4b7f243a6d9
Kanisk C <57884127+chakrakan@users.noreply.github.com>
2026-01-10 18:10:49 -0500

    adding some more places

--- src/consts.ts
+++ src/consts.ts
@@ -28,7 +28,7 @@ export const PROJECTS: Metadata = {
 export const REVIEWS: Metadata = {
   TITLE: "Reviews",
   DESCRIPTION:
-    "My reviews and thoughts on movies, TV shows, restaurants, books, and games.",
+    "My reviews and thoughts on movies, TV shows, restaurants, books, games, and anything else that can be reviewed.",
 };
 
 export const REVIEW_CATEGORIES = {
@@ -74,6 +74,8 @@ export const VISITED_PLACES = [
   { country: "USA", city: "Washington, D.C.", lat: 38.8951, lng: -77.0364 },
   { country: "USA", city: "Las Vegas", lat: 36.1699, lng: -115.1398 },
   { country: "USA", city: "Miami", lat: 25.7617, lng: -80.1918 },
+  { country: "USA", city: "Orlando", lat: 28.5383, lng: -81.3792 },
+  { country: "USA", city: "Joshua Tree", lat: 36.8522, lng: -116.3073 },
   { country: "Mexico", city: "Cancun", lat: 21.1619, lng: -86.8466 },
   
   // Europe
@@ -82,7 +84,7 @@ export const VISITED_PLACES = [
   { country: "Spain", city: "Valencia", lat: 39.4700, lng: -0.3769 },
   { country: "Spain", city: "Madrid", lat: 40.4168, lng: -3.7038 },
   { country: "Spain", city: "Granada", lat: 37.3891, lng: -5.9812 },
-  { country: "Spain", city: "Alicante", lat: 37.3891, lng: -5.9812 },
+  { country: "Spain", city: "Alicante", lat: 38.3452, lng: -0.4830 },
   { country: "Italy", city: "Rome", lat: 41.9028, lng: 12.4964 },
   { country: "Italy", city: "Milan", lat: 45.4642, lng: 9.1914 },
   { country: "Italy", city: "Venice", lat: 45.4414, lng: 12.3155 },
@@ -112,11 +114,43 @@ export const VISITED_PLACES = [
   { country: "Austria", city: "Innsbruck", lat: 47.2667, lng: 11.3833 },
   { country: "United Kingdom", city: "London", lat: 51.5074, lng: -0.1278 },
   { country: "Turkey", city: "Istanbul", lat: 41.0082, lng: 28.9784 },
+  { country: "Ukraine", city: "Kyiv", lat: 50.4501, lng: 30.5234 },
+  { country: "Luxembourg", city: "Luxembourg", lat: 49.6117, lng: 6.1319 },
 
   // Asia
   { country: "India", city: "New Delhi", lat: 28.6139, lng: 77.2090 },
   { country: "India", city: "Mumbai", lat: 19.0760, lng: 72.8777 },
   { country: "India", city: "Bangalore", lat: 12.9716, lng: 77.5946 },
-  { country: "India", city: "Hyderabad", lat: 17.3850, lng: 78.4867 },
   { country: "India", city: "Kolkata", lat: 22.5726, lng: 88.3639 },
+  { country: "India", city: "Mysore", lat: 12.3000, lng: 76.6500 },
+  { country: "India", city: "Agra", lat: 27.1767, lng: 78.0081 },
+  { country: "India", city: "Howrah", lat: 22.5726, lng: 88.3639 },
+  { country: "India", city: "Bhubaneswar", lat: 20.2961, lng: 85.8245 },
+  { country: "India", city: "Puri", lat: 19.8000, lng: 85.8167 },
+  { country: "India", city: "Jamshedpur", lat: 22.8000, lng: 86.1833 },
+  { country: "India", city: "Deoghar", lat: 24.4667, lng: 86.1500 },
+  { country: "India", city: "Mango", lat: 22.4939, lng: 86.1259 },
+  { country: "India", city: "Thiruvananthapuram", lat: 8.4867, lng: 76.9558 },
+  { country: "India", city: "Coimbatore", lat: 11.0167, lng: 76.9833 },
+
+  { country: "China", city: "Beijing", lat: 39.9042, lng: 116.4074 },
+  { country: "China", city: "Shanghai", lat: 31.2304, lng: 121.4737 },
+  { country: "China", city: "Zhangjiajie", lat: 29.1273, lng: 110.4788 },
+  { country: "China", city: "Qingdao", lat: 36.0671, lng: 120.3836 },
+  { country: "China", city: "Changsha", lat: 28.1839, lng: 112.9412 },
+  { country: "China", city: "Loudi", lat: 27.7367, lng: 112.0000 },
+
+  { country: "Singapore", city: "Singapore", lat: 1.3521, lng: 103.8198 },
+
+  // Middle East
+  { country: "United Arab Emirates", city: "Dubai", lat: 25.276987, lng: 55.296233 },
+  { country: "United Arab Emirates", city: "Abu Dhabi", lat: 24.4667, lng: 54.3667 },
+
+  // Africa
+  { country: "Egypt", city: "Cairo", lat: 30.0444, lng: 31.2357 },
+  { country: "Egypt", city: "Giza", lat: 30.0156, lng: 31.2065 },
+  { country: "Egypt", city: "Luxor", lat: 25.6872, lng: 32.6399 },
+  { country: "Egypt", city: "Sharm El-Sheikh", lat: 27.9772, lng: 34.2839 },
+  { country: "Egypt", city: "Hurghada", lat: 27.2514, lng: 33.8067 },
+
 ] as const;
